be1d9a81ce2dd98cbcf72b5d713ff62157733cf8
Jakhongir <jakhongir.nishanbaev@gmail.com>
2024-06-18 11:47:15 +0500

    refactor: change id type to string in products mock file

--- productService/lambda/mock/products.js
+++ productService/lambda/mock/products.js
@@ -1,17 +1,17 @@
 
 exports.products = [
     {
-        "id": 1,
+        "id": "1",
         "name": "Iphone 14",
         "price": 100
     },
     {
-        "id": 2,
+        "id": "2",
         "name": "Apple whatch",
         "price": 200
     },
     {
-        "id": 3,
+        "id": "3",
         "name": "IPhone headphones",
         "price": 300
     }
