59747987bb58b51d51e362b67f11563ddd26659b
Jakhongir <jakhongir.nishanbaev@gmail.com>
2024-06-20 09:56:57 +0500

    feat: add cors headers in response

--- productService/lambda/getProductsById.js
+++ productService/lambda/getProductsById.js
@@ -9,12 +9,22 @@ module.exports.handler = async (event) => {
     if (product !== undefined) {
         return {
             statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Origin": "*", // Allow from anywhere
+                "Access-Control-Allow-Methods": "GET" // Allow only GET request
+            },
             body: JSON.stringify(product)
         };
     }
 
     return {
         statusCode: 404,
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*", // Allow from anywhere
+            "Access-Control-Allow-Methods": "GET" // Allow only GET request
+        },
         body: JSON.stringify("Product not found.")
     };
 };
--- productService/lambda/getProductsList.js
+++ productService/lambda/getProductsList.js
@@ -3,6 +3,11 @@ module.exports.handler = async (event) => {
 
     return {
         statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*", // Allow from anywhere
+            "Access-Control-Allow-Methods": "GET" // Allow only GET request
+        },
         body: JSON.stringify(products.products)
     };
 };
